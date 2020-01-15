e7806b8eaea7f4c5021fce51de3cc93b771cd727
hamid <afshar.hamid91@gmail.com>
2020-01-15 15:39:44 +0330

    add elastic apm

--- service-a/src/main/java/com/afshar/servicea/ServiceAController.java
+++ service-a/src/main/java/com/afshar/servicea/ServiceAController.java
@@ -1,19 +1,28 @@
 package com.afshar.servicea;
 
+import co.elastic.apm.api.CaptureSpan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ServiceAController {
 
     public static Logger logger = LoggerFactory.getLogger(ServiceAController.class);
 
-    @RequestMapping(method = RequestMethod.GET, value ="api/status-a")
+    @Value("${prop}")
+    private String value;
+
+    @RequestMapping(method = RequestMethod.GET, value = "api/status-a")
+    @CaptureSpan
     public String getStatus() {
 
+        logger.info(value);
 //        logger.debug("Debug");
 //        logger.trace("Trace");
 //        logger.info("info");
